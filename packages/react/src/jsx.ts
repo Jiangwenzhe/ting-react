ab93d25f544b35e8976ef11d4263325a41ceefbc
Jiangwenzhe <jiangwenzhe2016@gmail.com>
2023-01-31 00:29:41 +0800

    feat: jsx 方法编写，RollUp 打包配置文件编写

---
+++ packages/react/src/jsx.ts
@@ -0,0 +1,94 @@
+import { REACT_ELEMENT_TYPE } from 'shared/ReactSymbols';
+import {
+	Type,
+	Key,
+	Ref,
+	Props,
+	ReactElementType,
+	ElementType
+} from 'shared/ReactTypes';
+
+// ReactElement
+const ReactElement = function (
+	type: Type,
+	key: Key,
+	ref: Ref,
+	props: Props
+): ReactElementType {
+	const element = {
+		$$typeof: REACT_ELEMENT_TYPE,
+		type,
+		key,
+		ref,
+		props,
+		__mark: 'CustomReact'
+	};
+	return element;
+};
+
+export const jsx = (type: ElementType, config: any, ...maybeChildren: any) => {
+	let key: Key = null;
+	const props: Props = {};
+	let ref: Ref = null;
+
+	for (const prop in config) {
+		const val = config[prop];
+		if (prop === 'key') {
+			if (val !== undefined) {
+				key = '' + key;
+			}
+			continue;
+		}
+		if (prop === 'ref') {
+			if (val !== undefined) {
+				ref = val;
+			}
+			continue;
+		}
+		if ({}.hasOwnProperty.call(config, prop)) {
+			props[prop] = val;
+		}
+	}
+	const maybeChildrenLength = maybeChildren.length;
+	if (maybeChildrenLength) {
+		if (maybeChildrenLength === 1) {
+			props.childern = maybeChildren[0];
+		} else {
+			props.childern = maybeChildren;
+		}
+	}
+	return ReactElement(type, key, ref, props);
+};
+
+/**
+ * 潇洒潇洒
+ * @param type
+ * @param config
+ * @returns {ReactElement}
+ */
+export const jsxDEV = (type: ElementType, config: any) => {
+	let key: Key = null;
+	const props: Props = {};
+	let ref: Ref = null;
+
+	for (const prop in config) {
+		const val = config[prop];
+		if (prop === 'key') {
+			if (val !== undefined) {
+				key = '' + val;
+			}
+			continue;
+		}
+		if (prop === 'ref') {
+			if (val !== undefined) {
+				ref = val;
+			}
+			continue;
+		}
+		if ({}.hasOwnProperty.call(config, prop)) {
+			props[prop] = val;
+		}
+	}
+
+	return ReactElement(type, key, ref, props);
+};
