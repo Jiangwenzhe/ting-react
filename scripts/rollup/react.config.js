1bfe2e7b77b3fbdcc0faae6b9fdafca6ba529113
Jiangwenzhe <jiangwenzhe2016@gmail.com>
2023-02-02 21:33:16 +0800

    feat: 完成 jsx 函数的编写

---
+++ scripts/rollup/react.config.js
@@ -0,0 +1,56 @@
+import {
+	getBaseRollUpPlugins,
+	getPackageJSON,
+	resolvePkgPath
+} from './utils.js';
+import generatePackageJson from 'rollup-plugin-generate-package-json';
+
+const { name, module } = getPackageJSON('react');
+// react 包路径
+const pkgPath = resolvePkgPath(name);
+// react 包产物路径
+const pkgDistPath = resolvePkgPath(name, true);
+
+export default [
+	// react 包
+	{
+		input: `${pkgPath}/${module}`,
+		output: {
+			file: `${pkgDistPath}/index.js`,
+			name: 'index.js',
+			format: 'umd'
+		},
+		plugins: [
+			...getBaseRollUpPlugins(),
+			generatePackageJson({
+				inputFolder: pkgPath,
+				outputFolder: pkgDistPath,
+				baseContents: ({ name, description, version }) => ({
+					name,
+					description,
+					version,
+					main: 'index.js'
+				})
+			})
+		]
+	},
+	// jsx-runtime
+	{
+		input: `${pkgPath}/src/jsx.ts`,
+		output: [
+			// jsx-runtime
+			{
+				file: `${pkgDistPath}/jsx-runtime.js`,
+				name: 'jsx-runtime.js',
+				format: 'umd'
+			},
+			// jsx-dev-runtime
+			{
+				file: `${pkgDistPath}/jsx-dev-runtime.js`,
+				name: 'jsx-dev-runtime.js',
+				format: 'umd'
+			}
+		],
+		plugins: [getBaseRollUpPlugins()]
+	}
+];
